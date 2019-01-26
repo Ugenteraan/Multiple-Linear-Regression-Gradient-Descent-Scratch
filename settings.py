aed76690c8c23a817cf26834d197803f54cae49e
Ugenteraan <m.ugenteraan15@gmail.com>
2019-01-26 20:21:23 +0800

    added screenshots

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 
 dataset_path = './dataset/dataset_csv.csv' #dataset path
 
-batch_size = 100
+batch_size = 1000
 
 learning_rate = 0.0000000001
 
-epoch = 20000
+epoch = 5000
 
 test_training_ratio = 0.2
